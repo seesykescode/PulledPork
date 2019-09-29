719970d875bc146d4598ca41f595dabee3e91e54
Johnathon Sykes <johnathon@seesykes.codes>
2019-09-29 13:56:54 -0700

    installed npm module the-traveller with test call

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const express = require('express')
-      app = express()
-      port = 3000
+app = express()
+port = 3000
+
 
 require('dotenv').config()
 
+var Traveler = require('the-traveler').default;
+const Enums = require('the-traveler/build/type-definitions')
 
 
-      app.get('/', (req, res) => {
-          res.send("It's lit, baby. Helllllooooooo WORLD")
-      })
+const traveler = new Traveler({
+    apikey: process.env.D2_API_KEY,
+    userAgent: 'pulledPork2.0'
+})
+
+
+app.get('/', (req, res) => {
+    res.send("It's lit, baby. Helllllooooooo WORLD")
+})
+
+app.get('/search/:platform/:memberID', (req, res) => {
+    platform = req.params.platform
+    playerName = req.params.memberID
+    playerObj =
+        traveler
+            .destiny2
+            .searchDestinyPlayer(platform, playerName)
+            .then(player => {
+                res.json(player)
+            })
+            .catch(err => res.send("yea it broke.", error))
+})
 
 
 
-      app.listen(port, () => console.log(`Express server is live and worldwide on port ${port}`))
+
+
+app.listen(port, () => console.log(`Express server is live and worldwide on port ${port}`))
 
