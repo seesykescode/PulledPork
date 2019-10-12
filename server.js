495aa6cfe4290c436aa393e4e62867e96c83693b
Johnathon Sykes <johnathon@seesykes.codes>
2019-10-12 06:20:54 -0700

    Removed 'the-traveler' package due to issues with oauth

--- server.js
+++ server.js
@@ -8,35 +8,6 @@ url = require('url')
 
 require('dotenv').config()
 
-var Traveler = require('the-traveler').default;
-const Enums = require('the-traveler/build/type-definitions')
-
-
-const traveler = new Traveler({
-    apikey: process.env.D2_API_KEY,
-    userAgent: 'pulledPork2.0'
-})
-
-
-app.get('/', (req, res) => {
-    res.send("It's lit, baby. Helllllooooooo WORLD")
-    
-})
-
-app.get('/auth', (req, res) => {
-   res.redirect(traveler.oauth.generateOAuthURL(process.env.D2_OAUTH_CLIENT))
-})
-
-app.get('/auth/callback', (req, res) => {
-    let code = req.query.code 
-    console.log(code)
-
-    traveler.oauth.getAccessToken(code).then(oauth => {
-        console.log(oauth)
-    }).catch(err=>{
-        console.log(err)
-    })
-})
 
 
 
